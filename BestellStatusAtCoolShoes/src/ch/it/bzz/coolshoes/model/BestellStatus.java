e5ead5ef5bc2c08df7aafb673fe1266d88fdaaf1
zlindl-bbc <luca.lindegger@gmx.ch>
2017-06-29 16:25:49 +0200

    Modelklassen

---
+++ BestellStatusAtCoolShoes/src/ch/it/bzz/coolshoes/model/BestellStatus.java
@@ -0,0 +1,54 @@
+package ch.it.bzz.coolshoes.model;
+
+import java.sql.Date;
+
+public class BestellStatus {
+
+  private int BSID;
+  private String Bestellnummer;
+  private String Status;
+  private Date Bearbeitung;
+  private Date Lieferunggeplant;
+
+  public int getBSID() {
+    return BSID;
+  }
+
+  public void setBSID(int bSID) {
+    BSID = bSID;
+  }
+
+  public String getBestellnummer() {
+    return Bestellnummer;
+  }
+
+  public void setBestellnummer(String bestellnummer) {
+    Bestellnummer = bestellnummer;
+  }
+
+  public String getStatus() {
+    return Status;
+  }
+
+  public void setStatus(String status) {
+    Status = status;
+  }
+
+  public Date getBearbeitung() {
+    return Bearbeitung;
+  }
+
+  public void setBearbeitung(Date bearbeitung) {
+    Bearbeitung = bearbeitung;
+  }
+
+  public Date getLieferunggeplant() {
+    return Lieferunggeplant;
+  }
+
+  public void setLieferunggeplant(Date lieferunggeplant) {
+    Lieferunggeplant = lieferunggeplant;
+  }
+
+
+}
