8bdef9633187313d89137932455ad8beb1db2cbe
zaschl-bbc <Lukas.Aschmann@bbcag.ch>
2017-07-04 16:45:14 +0200

    Tabelle
    Mein Versuch die Tabelle zu machen

--- BestellStatusAtCoolShoes/src/ch/it/bzz/coolshoes/controller/LoginController.java
+++ BestellStatusAtCoolShoes/src/ch/it/bzz/coolshoes/controller/LoginController.java
@@ -23,68 +23,69 @@ import ch.it.bzz.coolshoes.view.MainView;
 
 public class LoginController implements ActionListener, MouseListener {
 
-  private MitarbeiterDAO mitarbeiterDAO = new MitarbeiterJdbcDAO();
-  MainView mainView = new MainView();
-  
-  public void actionPerformed(ActionEvent e) {
-    JButton source = (JButton) e.getSource();
-    if (source.getText().equals("Login")) {
-      String forename = LoginView.getForenameField();
-      String password = LoginView.getPasswordField();
+	private MitarbeiterDAO mitarbeiterDAO = new MitarbeiterJdbcDAO();
+	MainView mainView = new MainView();
+	LoginView loginView = new LoginView();
 
-      if (StringUtils.isBlank(forename) || StringUtils.isBlank(password)) {
-        JOptionPane.showMessageDialog(null, "Bitte füllen sie alle Felder aus...", "Felder leer",
-            JOptionPane.WARNING_MESSAGE);
-      } else {
-        if (mitarbeiterDAO.doesExist(forename)) {
-          String dbPassword = mitarbeiterDAO.getPassword(forename);
-          if (password.equals(dbPassword)) {
-            // open Main (logged-in part)
-        	  mainView.initGui();
-        	  
-          } else {
-            JOptionPane.showMessageDialog(null, "Passwort falsch!", "Login-Error",
-                JOptionPane.INFORMATION_MESSAGE);
-          }
-        } else {
-          JOptionPane.showMessageDialog(null, "Benutzer wurde nicht gefunden!", "Login-Error",
-              JOptionPane.INFORMATION_MESSAGE);
-        }
-      }
-    } else {
-      LoginView.setBlankAndRequestInFocus();
-    }
-  }
+	public void actionPerformed(ActionEvent e) {
+		JButton source = (JButton) e.getSource();
+		if (source.getText().equals("Login")) {
+			String forename = LoginView.getForenameField();
+			String password = LoginView.getPasswordField();
 
-  @Override
-  public void mouseClicked(MouseEvent e) {
-    // open Register view
+			if (StringUtils.isBlank(forename) || StringUtils.isBlank(password)) {
+				JOptionPane.showMessageDialog(null, "Bitte füllen sie alle Felder aus...", "Felder leer",
+						JOptionPane.WARNING_MESSAGE);
+			} else {
+				if (mitarbeiterDAO.doesExist(forename)) {
+					String dbPassword = mitarbeiterDAO.getPassword(forename);
+					if (password.equals(dbPassword)) {
+						// open Main (logged-in part)
+						mainView.initGUI();
+						
 
-  }
+					} else {
+						JOptionPane.showMessageDialog(null, "Passwort falsch!", "Login-Error",
+								JOptionPane.INFORMATION_MESSAGE);
+					}
+				} else {
+					JOptionPane.showMessageDialog(null, "Benutzer wurde nicht gefunden!", "Login-Error",
+							JOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+		} else {
+			LoginView.setBlankAndRequestInFocus();
+		}
+	}
 
-  @Override
-  public void mouseEntered(MouseEvent e) {
-    // TODO Auto-generated method stub
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// open Register view
 
-  }
+	}
 
-  @Override
-  public void mouseExited(MouseEvent e) {
-    // TODO Auto-generated method stub
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-  }
+	}
 
-  @Override
-  public void mousePressed(MouseEvent e) {
-    // TODO Auto-generated method stub
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-  }
+	}
 
-  @Override
-  public void mouseReleased(MouseEvent e) {
-    // TODO Auto-generated method stub
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-  }
+	}
 
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
--- BestellStatusAtCoolShoes/src/ch/it/bzz/coolshoes/view/MainView.java
+++ BestellStatusAtCoolShoes/src/ch/it/bzz/coolshoes/view/MainView.java
@@ -1,31 +1,61 @@
 package ch.it.bzz.coolshoes.view;
 
 import java.awt.BorderLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+import javax.swing.DefaultCellEditor;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
-public class MainView extends JFrame{
-	
-	Object rowData[][] = { { "Row1-Column1", "Row1-Column2", "Row1-Column3"},
-			{ "Row2-Column1", "Row2-Column2", "Row2-Column3" } };
-	Object columnNames[] = { "ColumnOne", "Column Two", "Column Three" };
-	JTable table = new JTable(rowData, columnNames);
-	 
-//	public MainView(){
-//		initGui();
-//	}
-	
-	public void initGui(){
-		JFrame frame = new JFrame();
-		JScrollPane scrollPane = new JScrollPane(table);
-		
-		frame.add(scrollPane, BorderLayout.CENTER);
-		frame.setSize(500, 500);
-		frame.setVisible(true);
+import ch.it.bzz.coolshoes.view.ButtonRenderer;
+import ch.it.bzz.coolshoes.view.ButtonEditor;
+import ch.it.bzz.coolshoes.view.ComboBoxTableCellRenderer;;
+
+public class MainView extends JFrame {
+
+	public MainView() {
+		super("JButtonTable Example");
+
+		DefaultTableModel dm = new DefaultTableModel();
+		dm.setDataVector(new Object[][] { { "split", "foo" }, { "split", "bar" } },
+				new Object[] { "Button", "String" });
+
+		JTable table = new JTable(new TableModel());
+//		table.getColumn("Button").setCellRenderer(new ButtonRenderer());
+//		table.getColumn("Button").setCellEditor(new ButtonEditor(new JCheckBox()));
+		JScrollPane scroll = new JScrollPane(table);
+		getContentPane().add(scroll);
+		setSize(400, 300);
+		setVisible(false);
 	}
-	
-	
 
+	public void initGUI() {
+
+		MainView Mainframe = new MainView();
+		Mainframe.setVisible(true);
+	}
+
+	public void setUpStatusColumn(JTable table, TableColumn statusColumn) {
+		// Set up the editor for the sport cells.
+		JComboBox comboBox = new JComboBox();
+		comboBox.addItem("Bestellt");
+		comboBox.addItem("In Bearbeitung");
+		comboBox.addItem("Bereit für Lieferung");
+		comboBox.addItem("Lieferung");
+		comboBox.addItem("Abgeschlossen");
+		comboBox.addItem("None of the above");
+		statusColumn.setCellEditor(new DefaultCellEditor(comboBox));
+
+		// Set up tool tips for the sport cells.
+		DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
+		renderer.setToolTipText("Click for combo box");
+		statusColumn.setCellRenderer(renderer);
+	}
 }
